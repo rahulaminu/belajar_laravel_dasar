1c7f196dbaca6c6490efc3edc9012933a80d53d6
Rahul Aminu <rahul.aminu16@smk.belajar.id>
2024-11-19 16:58:44 +0800

    redesign ui

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About', 'nama' => 'Rahul Aminu']);
 });
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // $posts = Post::with(['author', 'category'])->latest()->get();
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -22,10 +24,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('author', 'category'); lazy eager loading
     return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // $posts = $category->posts->load('author', 'category'); lazy eager loading
     return view('posts', ['title' => ' Articles in Category: ' . $category->name, 'posts' => $category->posts]);
 });
 
